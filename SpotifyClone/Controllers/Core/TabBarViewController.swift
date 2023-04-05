21cd86662acc77de4bb87445e68c6091f50e2fed
ahmetkardesseven <ahmetkardesseven@gmail.com>
2023-04-05 17:33:12 +0300

    AppDelegate Ayarlandı

--- SpotifyClone/Controllers/Core/TabBarViewController.swift
+++ SpotifyClone/Controllers/Core/TabBarViewController.swift
@@ -29,11 +29,17 @@ class TabBarViewController: UITabBarController {
         let nav2 = UINavigationController(rootViewController: vc2)
         let nav3 = UINavigationController(rootViewController: vc3)
         
+        nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        nav3.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 1)
+        
+        
+        
         nav1.navigationBar.prefersLargeTitles = true
         nav2.navigationBar.prefersLargeTitles = true
         nav3.navigationBar.prefersLargeTitles = true
         
-        setViewControllers([nav1,nav2,nav3], animated: true)
+        setViewControllers([nav1,nav2,nav3], animated: false)
         
         
         
